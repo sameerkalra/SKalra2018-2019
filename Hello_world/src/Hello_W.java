fe093f1614962f135be028b37bd04c13f733b485
APCS3rd <APCS3rd@MHS-Dreyer-10>
2018-08-20 10:44:59 -0700

    First commit to github

---
+++ Hello_world/src/Hello_W.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author (Sameer Kalra) 
+ * @version (8/20/18)
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From <Sameer>"); //Display the string
+    }
+}
+
